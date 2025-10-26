15ee9df5847d473e7d6afc1ee0230251763e7398
Juan <juanawbest@gmail.com>
2025-10-26 16:36:57 +0800

    Done Tema Di Index.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen">
+        {/* Tombol Tema */}
+
+        {/* Efek Background */}
+
+        {/* Navbar */}
+
+        {/* Halaman Utama */}
+
+
+        {/* Footer */}
+    </div>
+  );
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div className="home-container">
+        <h1 className="text-4xl font-bold mb-4">Not Found</h1>
+    </div>
+  );
+}
