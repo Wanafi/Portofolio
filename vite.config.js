53e8fe389279d8ccbaa9d984a74da2337f674638
Juan <juanawbest@gmail.com>
2025-10-28 12:16:14 +0800

    Error Deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portofolio/',
+  base: '/portofolio/',
   plugins: [
     react(),
     tailwindcss(),
